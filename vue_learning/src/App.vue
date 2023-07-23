7908eae97de8680b77f04fe60cda8ee0ce6c204e
Zhang <12012724@mail.sustech.edu.cn>
2023-07-24 03:08:30 +0800

    first commit

---
+++ vue_learning/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <h3>vue初体验</h3>
+    <p>{{ msg }}</p>
+    <p>{{ num+1 }}</p>
+    <p>{{ ok?"yes":"no" }}</p>
+    <p>{{ message.split("").reverse().join("") }}</p>
+    <p>{{ rawHTML }}</p>
+    <p v-html="rawHTML"></p>
+</template>
+
+<script>
+  export default{
+    data(){
+      return {
+        msg:"helloworld",
+        num:10,
+        ok:true,
+        message:"大家好",
+        rawHTML:"<a href='http://www.baidu.com'>百度链接</a>"
+      }
+    }
+  }
+</script>
+
